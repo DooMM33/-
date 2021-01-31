4a4fbacf19486cea6e9b34796f64c4ebc9f30cdd
W5Wcy <792311540@qq.com>
2021-01-31 19:30:26 +0800

    today over

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <div id="building"/>
     <mainpic/>
+    <tabcontrol :showmess="['中奖纪录','活动规则','奖品设置']"/>
   </div>
 </template>
 
 <script>
 
 import mainpic from '@/components/mainpic/MainPic'
+import tabcontrol from '@/components/tabcontrol/TabControl'
 export default {
   name: 'App',
   components: {
-    mainpic
+    mainpic,
+    tabcontrol
   }
 }
 </script>
@@ -26,5 +29,4 @@ export default {
     background-size: 100% 100%;
     z-index: -1;
   }
-
 </style>
---
+++ src/components/tabcontrol/TabControl.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tabcontrol">
+    <div class="tab-control-item" v-for="item in showmess">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  props:{
+    showmess:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tabcontrol{
+    display:flex;
+    text-align: center;
+    height:40px;
+    line-height: 40px;
+    background-color: darkslateblue;
+    margin-top: 12%;
+  }
+  .tab-control-item{
+    flex: 1;
+  }
+  .tab-control-item span{
+    padding: 5px;
+    color:#ffffff;
+  }
+</style>
